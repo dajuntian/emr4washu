16c5854c468136936f717e8481beb82c793008bf
dajuntian <tiandajun@gmail.com>
2016-12-27 12:55:16 -0600

    first commit

---
+++ R/connect_db.R
@@ -0,0 +1,26 @@
+#' Connect to db2
+#' 
+#' This function allows to connect to db2 database
+#' @param host The host name of the database.
+#' @param port the port number of the database
+#' @param dbname the name of the database
+#' @param user the user name to log in
+#' @param pw the password for the user
+#' @export
+#' @examples 
+#' connect_db(host = "", port = "", user = "", pw = "")
+
+
+connect_db <- function(host, port, dbname, user, pw){
+  jcc = RJDBC::JDBC("com.ibm.db2.jcc.DB2Driver",
+                    "driver/db2jcc4.jar")
+  print(paste0("jdbc:db2://", host, ":", port, "/", "dbname"))
+  conn = RJDBC::dbConnect(jcc,
+                   paste0("jdbc:db2://", host, ":", port, "/", dbname),
+                   user = user,
+                   password = pw)
+  return(conn)
+}
+
+
+
