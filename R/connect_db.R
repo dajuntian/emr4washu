60fe0a4ea0d16aee97226314b817dbbf2eb7e8b7
dajuntian <tiandajun@gmail.com>
2016-12-27 13:21:03 -0600

    add path for driver

--- R/connect_db.R
+++ R/connect_db.R
@@ -1,6 +1,7 @@
 #' Connect to db2
 #' 
 #' This function allows to connect to db2 database
+#' @param driver the path to the jdbc driver
 #' @param host The host name of the database.
 #' @param port the port number of the database
 #' @param dbname the name of the database
@@ -11,9 +12,9 @@
 #' connect_db(host = "", port = "", user = "", pw = "")
 
 
-connect_db <- function(host, port, dbname, user, pw){
+connect_db <- function(driver, host, port, dbname, user, pw){
   jcc = RJDBC::JDBC("com.ibm.db2.jcc.DB2Driver",
-                    "driver/db2jcc4.jar")
+                    driver)
   print(paste0("jdbc:db2://", host, ":", port, "/", "dbname"))
   conn = RJDBC::dbConnect(jcc,
                    paste0("jdbc:db2://", host, ":", port, "/", dbname),
