6b7ef853ca74c98e703f14ee7811c45e18554bde
dajuntian <tiandajun@gmail.com>
2016-12-29 13:20:34 -0600

    add src

--- R/charlson.R
+++ R/charlson.R
@@ -9,9 +9,11 @@
 #' \dontrun{
 #' charlson(conn, "pt list.csv")
 #' }
-charlson <- function(conn, pt_list, look_back = 5) {
+charlson <- function(conn, pt_list, out_file = NA, look_back = 5) {
   pt <- utils::read.csv(pt_list)
   names(pt)[1] <-  "VISIT_NO"
+  #remove NA visit_no
+  pt <- pt[!is.na(pt$VISIT_NO), , drop = F]
 
   #delete and create session.candidate
   tryCatch(
@@ -30,46 +32,11 @@ charlson <- function(conn, pt_list, look_back = 5) {
   )
 
   #return the results from database
-  sql_get_icd_5 <-
-    "
-  select c.visit_no, icdx.ICDX_Diagnosis_Code, icdx.ICDX_Version_No
-  from session.candidate c
-  join cds.cds_visit index_cv
-  on c.visit_no = index_cv.visit_no
-  join cds.registration index_reg
-  on index_cv.reg_no = index_reg.reg_no
-  and index_cv.facility_concept_id = index_reg.facility_concept_id
-  join cds.registration pre_reg
-  on index_reg.reference_no = pre_reg.reference_no
-  and pre_reg.admit_date <= index_reg.admit_date
-  and pre_reg.admit_date >= (index_reg.admit_date - 5 years)
-  join cds.cds_visit pre_cv
-  on pre_reg.reg_no = pre_cv.reg_no
-  and pre_reg.facility_concept_id = pre_cv.facility_concept_id
-  join cds.registration_icdx_diagnosis icdx
-  on pre_cv.visit_no = icdx.visit_no
-  order by c.visit_no
-  "
-  sql_get_icd_1 <-
-    "
-  select c.visit_no, icdx.ICDX_Diagnosis_Code, icdx.ICDX_Version_No
-  from session.candidate c
-  join cds.cds_visit index_cv
-  on c.visit_no = index_cv.visit_no
-  join cds.registration index_reg
-  on index_cv.reg_no = index_reg.reg_no
-  and index_cv.facility_concept_id = index_reg.facility_concept_id
-  join cds.registration pre_reg
-  on index_reg.reference_no = pre_reg.reference_no
-  and pre_reg.admit_date <= index_reg.admit_date
-  and pre_reg.admit_date >= (index_reg.admit_date - 1 years)
-  join cds.cds_visit pre_cv
-  on pre_reg.reg_no = pre_cv.reg_no
-  and pre_reg.facility_concept_id = pre_cv.facility_concept_id
-  join cds.registration_icdx_diagnosis icdx
-  on pre_cv.visit_no = icdx.visit_no
-  order by c.visit_no
-  "
+  sql_get_icd_5 <- readChar("../src/sql_get_icd_5.sql", 
+                            file.info("../src/sql_get_icd_5.sql")$size)
+  
+  sql_get_icd_1 <- readChar("../src/sql_get_icd_1.sql",
+                            file.info("../src/sql_get_icd_1.sql")$size)
 
   if (look_back == 5) {
     sql_get_icd = sql_get_icd_5
@@ -91,43 +58,49 @@ charlson <- function(conn, pt_list, look_back = 5) {
                     all = T,
                     sort = T)
 
-  #remove space in the code
+  #remove dot and space in the code
+  pt_w_icd[, c(2)] <- sub("\\.", "", pt_w_icd[, c(2)])
   pt_w_icd[, c(2, 3)] <- sapply(pt_w_icd[, c(2, 3)], trimws)
-
 
   #get icd9 comorbidity
   pt_w_icd9 <- pt_w_icd
+  if (nrow(pt_w_icd9) > 0) {
   pt_w_icd9[!is.na(pt_w_icd9$ICDX_VERSION_NO) &
-              pt_w_icd9$ICDX_VERSION_NO == "10-CM", "ICDX_DIAGNOSIS_CODE"] <-
-    NA
-  pt_w_icd9$ICDX_VERSION_NO <- NULL
+              pt_w_icd9$ICDX_VERSION_NO == "10-CM", "ICDX_DIAGNOSIS_CODE"] <- NA
+  pt_w_icd9$ICDX_VERSION_NO <- NULL      
+  }
+
   com_pt9 <- icd::icd9_comorbid(
     pt_w_icd9,
     map = icd::icd9_map_quan_deyo,
     visit_name = "VISIT_NO",
     icd_name = "ICDX_DIAGNOSIS_CODE",
-    short_code = F
+    short_code = T,
+    short_map = T
   )
 
   #get icd10 comirbidity
   pt_w_icd10 <- pt_w_icd
+  if (nrow(pt_w_icd10)) {
   pt_w_icd10[!is.na(pt_w_icd10$ICDX_VERSION_NO) &
-               pt_w_icd10$ICDX_VERSION_NO == "9-CM", "ICDX_DIAGNOSIS_CODE"] <-
-    NA
+               pt_w_icd10$ICDX_VERSION_NO == "9-CM", "ICDX_DIAGNOSIS_CODE"] <- NA      
+  }
+
   pt_w_icd10$ICDX_VERSION_NO <- NULL
   com_pt10 <- icd::icd10_comorbid(
     pt_w_icd10,
     map = icd::icd10_map_quan_deyo,
     visit_name = "VISIT_NO",
     icd_name = "ICDX_DIAGNOSIS_CODE",
-    short_code = F
+    short_code = T,
+    short_map = T
   )
 
   #combine the results from 9 and 10
   com_pt <- com_pt9 | com_pt10
 
   score_pt <- icd::icd_charlson_from_comorbid(com_pt,
-                                              scoring_system = "charlson", hierarchy = T)
+                                              scoring_system = "quan", hierarchy = T)
   summary(score_pt)
 
   com_pt_n <- 1 * com_pt
@@ -135,12 +108,12 @@ charlson <- function(conn, pt_list, look_back = 5) {
   result <- cbind(pt, com_pt_n, score_pt)
 
   #the output file name
+  if (is.na(out_file)) {
   out_file <- paste0(normalizePath(dirname(pt_list)),
                      "\\",
                      "pt-w-charlson-",
                      sub("\\.", "", format(Sys.time(), "%Y%m%d%H%M%OS2")),
-                     ".csv")
+                     ".csv")}
   write.csv(result, out_file, row.names = F)
   cat(paste0("file output to ", out_file, "\n"))
-
 }
