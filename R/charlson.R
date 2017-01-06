8732f8a5e2fec5a2cd41c07242b432e803b36622
dajuntian <tiandajun@gmail.com>
2017-01-05 20:46:01 -0600

    major revise

--- R/charlson.R
+++ R/charlson.R
@@ -10,17 +10,17 @@
 #' charlson(conn, "user.incohort", "usr.out_cohort")
 #' }
 # charlson <- function(conn, pt_list, out_file = NA, look_back = 5) {
-charlson <- function(conn, inData, outData, look_back = 5) {    
+charlson <- function(conn, inData, outData) {
   sql_get_icd_5 <- paste0("
     select distinct c.visit_no, icdx.ICDX_Diagnosis_Code, icdx.ICDX_Version_No,
-    case when 
+    case when
                           pre_reg.admit_date >= (index_reg.admit_date - 1 years) then 1
                           else 5
                           end as year_back,
                           floor( (days(index_reg.discharge_date)-days(p.dob))/365.25) AS age
-                          
+
                           from ",
-    inData, 
+    inData,
  " c
 join cds.cds_visit index_cv
  on c.visit_no = index_cv.visit_no
@@ -33,26 +33,27 @@ join cds.cds_visit index_cv
  on index_reg.reference_no = pre_reg.reference_no
  and pre_reg.admit_date <= index_reg.admit_date
  and pre_reg.admit_date >= (index_reg.admit_date - 5 years)
- join cds.cds_visit pre_cv   
+ join cds.cds_visit pre_cv
  on pre_reg.reg_no = pre_cv.reg_no
  and pre_reg.facility_concept_id = pre_cv.facility_concept_id
  join cds.registration_icdx_diagnosis icdx
  on pre_cv.visit_no = icdx.visit_no
  order by c.visit_no
-  ")  
-  
+  ")
+
   sql_get_icd = sql_get_icd_5
 
 
   pt_w_icd <- RJDBC::dbGetQuery(conn, sql_get_icd)
   result_5 <- .help_df2score(pt_w_icd, 5)
-  
+
   #get results for one year look back
   pt_w_icd[pt_w_icd$YEAR_BACK == 5, "ICDX_DIAGNOSIS_CODE"] <- NA
   result_1 <- .help_df2score(pt_w_icd, 1)
-  
+
+  result <- merge(result_1, result_5)
   #output data to user
-  dbWriteTable(conn, outData, result_5)
+  RJDBC::dbWriteTable(conn, outData, result)
 
   cat(paste0("\ndata output to ", outData, "\n"))
 }
@@ -60,15 +61,15 @@ join cds.cds_visit index_cv
 .help_df2score <- function(pt_w_icd, look_back) {
     pt_w_icd[, c(2)] <- sub("\\.", "", pt_w_icd[, c(2)])
     pt_w_icd[, c(2, 3)] <- sapply(pt_w_icd[, c(2, 3)], trimws)
-    
+
     #get icd9 comorbidity
     pt_w_icd9 <- pt_w_icd
     if (nrow(pt_w_icd9) > 0) {
         pt_w_icd9[!is.na(pt_w_icd9$ICDX_VERSION_NO) &
                       pt_w_icd9$ICDX_VERSION_NO == "10-CM", "ICDX_DIAGNOSIS_CODE"] <- NA
-        pt_w_icd9$ICDX_VERSION_NO <- NULL      
+        pt_w_icd9$ICDX_VERSION_NO <- NULL
     }
-    
+
     com_pt9 <- icd::icd9_comorbid(
         pt_w_icd9,
         map = icd::icd9_map_quan_deyo,
@@ -77,14 +78,14 @@ join cds.cds_visit index_cv
         short_code = T,
         short_map = T
     )
-    
+
     #get icd10 comirbidity
     pt_w_icd10 <- pt_w_icd
     if (nrow(pt_w_icd10)) {
         pt_w_icd10[!is.na(pt_w_icd10$ICDX_VERSION_NO) &
-                       pt_w_icd10$ICDX_VERSION_NO == "9-CM", "ICDX_DIAGNOSIS_CODE"] <- NA      
+                       pt_w_icd10$ICDX_VERSION_NO == "9-CM", "ICDX_DIAGNOSIS_CODE"] <- NA
     }
-    
+
     pt_w_icd10$ICDX_VERSION_NO <- NULL
     com_pt10 <- icd::icd10_comorbid(
         pt_w_icd10,
@@ -94,19 +95,19 @@ join cds.cds_visit index_cv
         short_code = T,
         short_map = T
     )
-    
+
     #combine the results from 9 and 10
     com_pt <- com_pt9 | com_pt10
-    
+
     score_pt <- icd::icd_charlson_from_comorbid(com_pt,
                                                 scoring_system = "charlson", hierarchy = T)
     summary(score_pt)
-    
+
     com_pt_n <- 1 * com_pt
     visit_no <- pt_w_icd[!duplicated(pt_w_icd$VISIT_NO), c("VISIT_NO", "AGE")]
     result <- cbind(data.frame(VISIT_NO = unique(pt_w_icd$VISIT_NO)),
                     com_pt_n, score_pt)
-    
+
     #join result and the age;
     result <- merge(result, visit_no, all = T)
     result$score <- result$score_pt
@@ -123,12 +124,13 @@ join cds.cds_visit index_cv
     result$score_pt <- NULL
     result$AGE <- NULL
     result$score_age_adj <- result$score_age_adj + result$score
-    
+
     if (look_back == 5) {
         names(result)[2:20] <- paste0("five_year_", names(result[2:20]))
     } else {
         names(result)[2:20] <- paste0("one_year_", names(result[2:20]))
     }
-    
+
     return(result)
 }
+
