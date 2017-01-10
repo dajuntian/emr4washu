271f745107c2b3999e0f56bf84802bec88a12a84
dajuntian <tiandajun@gmail.com>
2017-01-10 16:30:20 -0600

    change

---
+++ R/parse_sql_comments.R
@@ -0,0 +1,13 @@
+#' remove comments from sql statements
+#'
+#' This function is used to parse comments from sql statments. It removes something like /* coments*/ or
+#' --. But it won't work if there is something like select * from tname where x = '--test'.
+#' @param input_sql the statements of sql in string variable
+#' @export
+#' @examples 
+#' parse_sql_comments("select * from t --select \n where t <2")
+
+parse_sql_comments <- function(input_sql) {
+    gsub("(--.*)|(((/\\*)+?[\\w\\W]+?(\\*/)+))", " ", input_sql, perl = T)
+}
+
